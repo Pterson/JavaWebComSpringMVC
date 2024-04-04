c6aeb90452cd587e9d139be6e64a32674eb730cf
Peterson Ananias <peterson.anania@outlook.com>
2024-04-04 20:09:30 -0300

    Hello Servlet

--- Proj_HelloServlet/src/main/java/controller/HelloWordServlet.java
+++ Proj_HelloServlet/src/main/java/controller/HelloWordServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class helloWordServlet extends HttpServlet {
+public class HelloWordServlet extends HttpServlet {
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
