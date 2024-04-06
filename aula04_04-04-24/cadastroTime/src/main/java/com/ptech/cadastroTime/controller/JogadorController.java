623dd78f30c93df9620f70ebf93f55c8c7da9dd1
Peterson Ananias <peterson.anania@outlook.com>
2024-04-05 22:05:05 -0300

    continuando POST e inclusao Banco de Dados

--- aula04_04-04-24/cadastroTime/src/main/java/com/ptech/cadastroTime/controller/JogadorController.java
+++ aula04_04-04-24/cadastroTime/src/main/java/com/ptech/cadastroTime/controller/JogadorController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ptech.cadastroTime.model.Jogador;
+import com.ptech.cadastroTime.persistence.DaoJogador;
 
 @Controller
 public class JogadorController {
@@ -18,6 +19,10 @@ public class JogadorController {
 	@PostMapping("/jogador")
 	public String cadastroJogadorPost(Jogador jogador) {
 
+		//Invocando a Dao para salvar o jogador no banco	
+		DaoJogador daoJogador = new DaoJogador();
+		daoJogador.salvarJogador(jogador);		
+		
 		System.out.println(jogador.getNome());
 		System.out.println(jogador.getTime());
 		return "cadastroJogador";
