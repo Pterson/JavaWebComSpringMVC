6581dbc188b9fb0b9380b11e6fe6e6746574d4b7
Peterson Ananias <peterson.anania@outlook.com>
2024-04-14 21:49:20 -0300

    Construção de api utilizando GET e POST (Cadastrar, Listar, buscar pelo Id) - versão 2.0

--- aula10_12-04-24/cadastroPessoa/src/main/java/com/ptech/com/cadastroPessoa/controller/PessoaCadastroController.java
+++ aula10_12-04-24/cadastroPessoa/src/main/java/com/ptech/com/cadastroPessoa/controller/PessoaCadastroController.java
@@ -1,30 +1,38 @@
 package com.ptech.com.cadastroPessoa.controller;
 
+
+import com.ptech.com.cadastroPessoa.exception.ResourceNotFoundException;
 import com.ptech.com.cadastroPessoa.model.PessoaCadastro;
 import com.ptech.com.cadastroPessoa.repository.PessoaCadastroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PessoaCadastroController {
 
     @Autowired
     PessoaCadastroRepository pessoaCadastroRepository;
-
-    @GetMapping("/recebeDadosApi")
-    public String exibirMinhaApi() {
-
-        return "Im Working Mister Peterson";
+    @PostMapping("/enviarCadastroPessoa") // Mapeia o endpoint para cadastro de pessoa via POST
+    public String cadastrarPessoa(@RequestBody PessoaCadastro pessoaCadastro) { //@ResponseBody - deu erro
+        pessoaCadastroRepository.save(pessoaCadastro);
+        return "A pessoa " + PessoaCadastro.getNome() + " foi cadastrada";
     }
-    @PostMapping("/enviaDadosApi")
-    public String enviarParaApi(@RequestBody PessoaCadastro pessoaCadastroPost) {
+    @GetMapping("/exibirCadastroPessoa")
+    public List<PessoaCadastro> exibirPessoa(){
 
-        pessoaCadastroRepository.save(pessoaCadastroPost);
-
-        return "Ok, Mister Peterson, o usuário:" + pessoaCadastroPost.getNome() + " foi cadastrado";
+        return pessoaCadastroRepository.findAll();
     }
 
+    @GetMapping("/buscarCadastroPessoa/{id}")
+    public ResponseEntity<PessoaCadastro> getUsersById(@PathVariable(value = "id") long pessoaId) // Método para buscar uma pessoa pelo ID
+                 throws ResourceNotFoundException {
+          PessoaCadastro pessoaCadastro = pessoaCadastroRepository // Busca a pessoa pelo ID utilizando o repositório
+                 .findById(String.valueOf(pessoaId)) // Método para buscar pelo ID
+                 .orElseThrow(() -> new ResourceNotFoundException("Não existe pessoa com o ID: " + pessoaId)); // Lança uma exceção caso a pessoa não seja encontrada
+        return ResponseEntity.ok().body(pessoaCadastro); // Retorna a pessoa encontrada com status OK
+
+        }
 }
--- aula10_12-04-24/cadastroPessoa/src/main/java/com/ptech/com/cadastroPessoa/model/PessoaCadastro.java
+++ aula10_12-04-24/cadastroPessoa/src/main/java/com/ptech/com/cadastroPessoa/model/PessoaCadastro.java
@@ -1,30 +1,28 @@
 package com.ptech.com.cadastroPessoa.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class PessoaCadastro {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO) // Informa que vai gerar automatico
+    @GeneratedValue(strategy = GenerationType.AUTO) // Informa que vai gerar automático
     private long id;
-    private String nome;
+    private static String nome; //colocar STATIC
     private String email;
     private String cpf;
 
-    public String getNome() {
+    public static String getNome() {
         return nome;
     }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    public static void setNome(String nome) {
+        PessoaCadastro.nome = nome;
     }
 
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
@@ -32,7 +30,6 @@ public class PessoaCadastro {
     public String getCpf() {
         return cpf;
     }
-
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
